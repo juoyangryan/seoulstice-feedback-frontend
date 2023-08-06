d5678c2fb7dac0288b44e22211c36c55b3194bd2
juoyangryan <juoyang@gatech.edu>
2023-08-05 22:44:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe src="https://drive.google.com/file/d/1AdJQKhHYSzlXs_AlCZvarCl8YcHMmT9o/preview" width="80%" height="600" allow="autoplay"></iframe>
       </header>
     </div>
   );
